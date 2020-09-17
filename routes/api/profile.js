121d3566c644033785536a9b9192753d877b243b
Chris Haack <chris.haack@me.com>
2020-09-17 15:08:44 -0400

    add public routes to get all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -30,7 +30,6 @@ router.get('/me', auth, async (req, res) => {
 // @route   Post api/profile
 // @desc    Create or update user profile
 // @access  Private
-
 router.post(
   '/',
   [
@@ -101,5 +100,18 @@ router.post(
     }
   }
 );
+
+// @route   Get api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
