01d4cf7148084bc5d6d2901d547d913b4a785045
Chris Haack <chris.haack@me.com>
2020-10-09 12:15:42 -0400

    add login action, redirect to dashboard in both login and register components if user is authenticated

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
 import register from './auth';
+import auth from './auth';
 
 export default combineReducers({
   alert,
+  auth,
   register
 });
