0daf4591f653a780e7db2d869196c24732a01e08
Chris Haack <chris.haack@me.com>
2020-10-14 12:54:20 -0400

    add getCurrentProfile action/reducer; connect to Dashboard component

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import register from './auth';
 import auth from './auth';
+import profile from './profile';
 
 export default combineReducers({
   alert,
   auth,
-  register
+  register,
+  profile
 });
